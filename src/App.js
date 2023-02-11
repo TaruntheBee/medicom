b81f951352c09bdee44ef84eeb25c85de867ab67
xendai66 <114280549+xendai66@users.noreply.github.com>
2023-02-11 12:48:21 +0530

    commit Header of React

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import  React from 'react';
+import './App.css';
+import Header from "./Header";
+function App() {
+  return ( 
+    <div className="app">
+
+  {/*Header*/}
+  <Header />
+
+  {/* App Body */}
+  {/* SideBar */}
+  {/* Feed */}
+  {/* Widgets */}  
+    </div>
+   
+);
+}
+
+export default App;
