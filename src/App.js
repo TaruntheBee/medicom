7c0da8bfd94a764aaf915395a52b85950822b346
xendai66 <114280549+xendai66@users.noreply.github.com>
2023-02-11 22:36:04 +0530

    Sidebar done!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import  React from 'react';
 import './App.css';
 import Header from "./Header";
-import Sidebar from "./Sidebar";
+import SideBar from "./Sidebar";
 function App() {
   return ( 
     <div className="app">
@@ -10,8 +10,8 @@ function App() {
   {/* App Body */}
   <div className="app_body">
     <SideBar/>
-  {/* Feed */}
-  {/* Widgets */} 
+  {/* Feed ,
+   Widgets */} 
   </div> 
 </div> 
 );
