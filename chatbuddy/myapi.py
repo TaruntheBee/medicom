bb6a75f3c049942f1babab916916ad0376904454
Tarun Singh <hackerurfta@gmail.com>
2023-02-12 09:19:17 +0530

    virtual assistant is updated

--- chatbuddy/myapi.py
+++ chatbuddy/myapi.py
@@ -14,24 +14,6 @@ model_engine = "text-ada-001"
 
 app = FastAPI()
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
-
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: int):
-#     return {"item_id": item_id}
-
-# @app.get("/users/me")
-# async def read_user_me():
-#     return {"user_id": "the current user"}
-
-
-# @app.get("/users/{user_id}")
-# async def read_user(user_id: str):
-#     return {"user_id": user_id}
-
-
 def buddy(str):
     #option=str
     prompt = "symptoms of "+ str
@@ -42,17 +24,13 @@ def buddy(str):
     max_tokens=35,
     n=1,
     stop=None,
-    temperature=0.2,
-    
+    temperature=0.2,  
     )
     # extracting useful part of response
     response = completion.choices[0].text
     #print(response)
   
     return{response}
-
-def learn_to_code():
-    print("You can learn to code for free on freeCodeCamp")
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
